45ece9f21f91feb9d3d18bc2c2777cd4e05bb442
Angela L L <98534270+achen153@users.noreply.github.com>
2022-03-22 13:55:37 +0000

    ff

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
 #import your controller
 
+def list_maker():
+  mylist = [ ]
+  for i in range(1,5):
+    a_number = int(input("enter a number"))
+    mylist.append(a_number)
+  return mylist
+
 def main():
     pygame.init()
+    listmaker()
     #Create an instance on your controller object
     #Call your mainloop
     
